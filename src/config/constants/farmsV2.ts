1b12f461824ae84ad9c975e49e09cde845ecbd7e
Demari C <demaricameron@outlook.com>
2022-04-06 02:48:40 -0400

    test

--- src/config/constants/farmsV2.ts
+++ src/config/constants/farmsV2.ts
@@ -6,8 +6,8 @@ const farmsV2: FarmConfig[] = [
     pid: 1,
     lpSymbol: 'SOKU-ETH LP',
     lpAddresses: {
-      4: '',
       1: '0x0de56b00b45a07b15Ca09661336f785E7E4D1d45',
+      4: '0x6812108E96f2d53680446aF1dE8C6d474bD5b610',
     },
     token: tokens.soku,
     quoteToken: tokens.eth,
@@ -16,8 +16,8 @@ const farmsV2: FarmConfig[] = [
     pid: 2,
     lpSymbol: 'SUTEKU-ETH LP',
     lpAddresses: {
-      4: '',
       1: '0x9C5465026C45152bCfBA70084cb1CCC065d691ca',
+      4: '0x6812108E96f2d53680446aF1dE8C6d474bD5b610',
     },
     token: tokens.suteku,
     quoteToken: tokens.eth,
