513283a13f8220d9af5be72373c96e56fe560a07
ChefMari <demaricameron@outlook.com>
2022-08-11 08:59:19 -0400

    feat: update rpc

--- src/utils/getRpcUrl.ts
+++ src/utils/getRpcUrl.ts
@@ -1,24 +1,13 @@
 import random from 'lodash/random'
 
 // Array of available nodes to connect to
-export const nodes = [
-  'https://bsc-dataseed2.defibit.io/',
-  'https://bsc-dataseed3.defibit.io/',
-  'https://bsc-dataseed4.defibit.io/',
-  'https://bsc-dataseed2.ninicoin.io/',
-  'https://bsc-dataseed3.ninicoin.io/',
-  'https://bsc-dataseed4.ninicoin.io/',
-  'https://bsc-dataseed1.binance.org/',
-  'https://bsc-dataseed2.binance.org/',
-  'https://bsc-dataseed3.binance.org/',
-  'https://bsc-dataseed4.binance.org/',
-]
+export const nodes = ['https://eth-rpc.gateway.pokt.network']
 
 // console.log(process.env, 'dotenv')
 
 const getNodeUrl = () => {
   const randomIndex = random(0, nodes.length - 1)
-  return 'https://speedy-nodes-nyc.moralis.io/a80a9a59f7e3ae9405c48919/eth/mainnet'
+  return 'https://eth-rpc.gateway.pokt.network'
 }
 
 export default getNodeUrl
