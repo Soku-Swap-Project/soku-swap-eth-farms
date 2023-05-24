0b17b00b8996a778d84cf8a93523af12d3e4c5a4
ChefMari <demaricameron@outlook.com>
2023-05-23 21:33:11 -0400

    update rpc list

--- src/utils/getRpcUrl.ts
+++ src/utils/getRpcUrl.ts
@@ -1,13 +1,19 @@
 import random from 'lodash/random'
 
 // Array of available nodes to connect to
-export const nodes = ['https://mainnet.infura.io/v3/f55b065f57ad4ea4bf2c81b38da48c46']
+export const nodes = [
+  'https://rpc.ankr.com/eth	',
+  'https://eth.llamarpc.com',
+  'https://rpc.ankr.com/eth',
+  'https://endpoints.omniatech.io/v1/eth/mainnet/public	',
+  'https://1rpc.io/eth',
+]
 
 // console.log(process.env, 'dotenv')
 
 const getNodeUrl = () => {
   const randomIndex = random(0, nodes.length - 1)
-  return 'https://mainnet.infura.io/v3/f55b065f57ad4ea4bf2c81b38da48c46'
+  return nodes[randomIndex]
 }
 
 export default getNodeUrl
